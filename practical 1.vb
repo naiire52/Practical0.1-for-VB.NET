ebbe94133d0ae1fccce154440dd3c139f4c9feaa
naiire52 <134964233+naiire52@users.noreply.github.com>
2023-06-27 05:14:27 -0700

    Add files via upload

---
+++ practical 1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("You've succesfully logged in", "HIO IMEWEZA")
+        Dim r As VariantType
+        r = MessageBox.Show("Are you sure you want to close the application?", "CONFIRM EXIT", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If r = vbYes Then
+            Me.Close()
+        End If
+    End Sub
+End Class
